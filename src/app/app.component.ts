03ef1f213c2da625eb0d3638968596d949965a48
Michał Hadamik <m.hadamik@codelabs.pl>
2023-06-04 20:06:43 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  id = 1393;
+  config: any;
+
+  constructor(private readonly appService: AppService) {}
+
+  ngOnInit(): void {
+    this.appService.getMediaFile(this.id).subscribe((data) => {
+      this.config = data;
+    });
+  }
 }
